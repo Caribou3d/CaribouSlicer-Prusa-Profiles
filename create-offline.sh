f36e2e02392656b8edbbb0c985c42d8ace933c56
wschadow <wolfgang.schadow@t-online.de>
2025-05-30 11:44:14 +0200

    updated scripts

--- create-offline.sh
+++ create-offline.sh
@@ -17,7 +17,8 @@ echo "[INFO] Preparing offline directory..."
 rm -rf "$offline_dir"
 mkdir -p "$offline_dir"
 
-folders=("caribou-fff" "non-caribou-fff" "non-caribou-sla")
+# Process non-caribou folders normally
+folders=("non-caribou-fff" "non-caribou-sla")
 
 declare -A names=(
   [caribou-fff]="Caribou FFF"
@@ -76,9 +77,58 @@ EOF
 
   echo "[INFO] Zipping complete folder: $folder"
   zip_name="${folder}-offline.zip"
-  zip -rq "../$zip_name" ./*
+  zip -rq "../$zip_name" ./
   popd > /dev/null
 
 done
+
+# Special case for caribou-fff
+folder="caribou-fff"
+echo "[INFO] Processing: $folder (special case)"
+src_folder="$download_dir/$folder"
+dest_folder="$offline_dir/$folder"
+
+cp -r "$src_folder" "$dest_folder"
+rm -f "$dest_folder/vendor_indices.zip" "$dest_folder/manifest.json"
+
+idx_file=$(find "$dest_folder" -type f -name "*.idx" | head -n 1)
+vendor_name=$(basename "$idx_file" .idx)
+version=$(awk 'NR==2 {print $1; exit}' "$idx_file")
+ini_file="$download_dir/$folder/Caribou/$version.ini"
+cp "$ini_file" "$(dirname "$idx_file")/Caribou.ini"
+
+find "$dest_folder" -type f -name "*.ini" | while read -r ini_file; do
+  vendor_dir=$(basename "$(dirname "$ini_file")")
+  sed -i 's|config_update_url = .*|config_update_url = https://caribou3d.com/CaribouSlicer/preset-repo/settings-master/'"$vendor_dir"'/|g' "$ini_file"
+  sed -i 's|^[#[:space:]]*changelog_url[[:space:]]*=.*|# changelog_url =|g' "$ini_file"
+  sed -i 's|files.prusa3d.com/wp-content/uploads/repository/PrusaSlicer-settings-master/live/|caribou3d.com/CaribouSlicer/repository/vendors/|g' "$ini_file"
+  sed -i 's/ PrusaSlicer/ CaribouSlicer/g' "$ini_file"
+  sed -i 's/non-prusa-fff/non-caribou-fff/g' "$ini_file"
+  sed -i 's/prusa-fff/non-caribou-fff/g' "$ini_file"
+  sed -i 's/non-prusa-sla/non-caribou-sla/g' "$ini_file"
+  sed -i 's/prusa-sla/non-caribou-sla/g' "$ini_file"
+done
+
+pushd "$dest_folder" > /dev/null
+if ls *.idx 1> /dev/null 2>&1; then
+  zip -o vendor_indices.zip *.idx Caribou.ini
+  rm -f *.idx Caribou.ini
+fi
+
+cat > "manifest.json" <<EOF
+{
+  "name": "${names[$folder]}",
+  "description": "${descriptions[$folder]}",
+  "visibility": "",
+  "id": "$folder",
+  "url": "https://caribou3d.com/CaribouSlicer/preset-repo/$folder",
+  "index_url": "https://caribou3d.com/CaribouSlicer/preset-repo/$folder/vendor_indices.zip",
+  "offline_archive_url": "https://caribou3d.com/CaribouSlicer/Releases/00_Offline-Profiles/${folder}-offline.zip"
+}
+EOF
+
+zip_name="${folder}-offline.zip"
+zip -rq "../$zip_name" ./
+popd > /dev/null
 
 echo "✅ Offline preset package ready in: $offline_dir"
